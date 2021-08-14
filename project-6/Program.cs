c6f31bafc250c23fbd3fd8e69741bdaa00e80ad8
Jon Hutchings <sixty.mage@outlook.com>
2021-08-14 16:18:06 +0200

    Add project-6 with solution

---
+++ project-6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace project_6
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      const long n = 100;
+      long sum = CalcSum(n);
+      long sumOfSquares = CalcSumOfSquares(n);
+      long squareOfSum = sum * sum;
+      long difference = squareOfSum - sumOfSquares;
+      Console.WriteLine($"Difference (∑k)² - ∑(k²) (kϵ1..10): {squareOfSum} - {sumOfSquares} = {difference}");
+    }
+
+    private static long CalcSum(long n)
+    {
+      return (n * (n + 1)) / 2;
+    }
+
+    private static long CalcSumOfSquares(long n)
+    {
+      return (n * (n + 1) * (2 * n + 1)) / 6;
+    }
+  }
+}
