27fe5c846b6658a734e43fb64b526a4c5f69f52a
Jon Hutchings <sixty.mage@outlook.com>
2021-08-21 13:39:27 +0200

    Add solution

---
+++ problem-8/Reducer.cs
@@ -0,0 +1,31 @@
+﻿namespace problem_8
+{
+  public static class SequenceComparer
+  {
+    public static int Compare(Sequence sequence1, Sequence sequence2)
+    {
+      var seq1 = new Sequence(sequence1);
+      var seq2 = new Sequence(sequence2);
+
+      while (true)
+      {
+        var match = seq1.FindMatch(seq2);
+
+        if (match != -1)
+        {
+          seq1.Remove(match);
+          seq2.Remove(match);
+        }
+        else
+        {
+          break;
+        }
+      }
+
+      var product1 = seq1.Product;
+      var product2 = seq2.Product;
+
+      return product1 > product2 ? 1 : product1 < product2 ? -1 : 0;
+    }
+  }
+}
---
+++ problem-8/Sequence.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace problem_8
+{
+  public class Sequence
+  {
+    public int Count { get { return _digits.Count; } }
+
+    private readonly List<long> _digits;
+
+    public long Product
+    {
+      get
+      {
+        long product = 1;
+        _digits.ForEach(x => product = product * x);
+        return product;
+      }
+    }
+
+    public Sequence(IEnumerable<char> digits)
+    {
+      _digits = new List<long>();
+      foreach (char digit in digits)
+      {
+        if (Int64.TryParse(digit.ToString(), out long result))
+        {
+          _digits.Add(result);
+        }
+      }
+    }
+
+    public Sequence(Sequence sequence)
+    {
+      this._digits = new List<long>(sequence._digits);
+    }
+
+    public override string ToString()
+    {
+      return string.Join("", _digits);
+    }
+
+    public bool Contains(long digit)
+    {
+      return _digits.Contains(digit);
+    }
+
+    public long FindMatch(Sequence sequence)
+    {
+      foreach (var digit in _digits)
+      {
+        if (sequence.Contains(digit))
+        {
+          return digit;
+        }
+      }
+
+      return -1;
+    }
+
+    public void Remove(long digit)
+    {
+      _digits.Remove(digit);
+    }
+  }
+}
