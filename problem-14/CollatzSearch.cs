1e26e23b8ecb244686a985d61c8a1f524617bf46
Jon Hutchings <sixty.mage@outlook.com>
2021-12-28 15:17:54 +0200

    Add solution for problem-14

---
+++ problem-14/CollatzSearch.cs
@@ -0,0 +1,61 @@
+﻿namespace problem_14
+{
+  internal class CollatzSearch
+  {
+    private readonly Dictionary<long, long> _collatz = new Dictionary<long, long>();
+
+    public void Populate(long n)
+    {
+      long current = n;
+
+      while (true)
+      {
+        if (_collatz.ContainsKey(current))
+        {
+          break;
+        }
+
+        var next = Next(current);
+        _collatz[current] = next;
+        current = next;
+      }
+    }
+
+    public List<long> GetSequence(long n)
+    {
+      if (n < 0 )
+      {
+        return new List<long>();
+      }
+
+      if (!_collatz.ContainsKey(n))
+      {
+        Populate(n);
+      }
+
+      var sequence = new List<long>();
+      while (true)
+      {
+        sequence.Add(n);
+        if (n == 1)
+        {
+          break;
+        }
+
+        n = _collatz[n];
+      }
+
+      return sequence;
+    }
+
+
+    private static long Next(long n)
+    {
+      if (n % 2 == 0)
+      {
+        return n / 2;
+      }
+      return n * 3 + 1;
+    }
+  }
+}
--- problem-14/Program.cs
+++ problem-14/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace problem_14
+{ 
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      const int searchSpace = 1000000;
+      var collatzSearch = new CollatzSearch();
+
+      var maxSequence = new List<long>();
+
+      for (long n = 1; n <= searchSpace; n++)
+      {
+        var sequence = collatzSearch.GetSequence(n);
+        if (sequence.Count > maxSequence.Count)
+        {
+          maxSequence = sequence;
+        }
+      }
+
+      Console.WriteLine($"{maxSequence.First()}: {maxSequence.Count} ({SequenceDescription(maxSequence, 10)})");
+    }
+
+    private static string SequenceDescription(List<long> sequence, int maxTerms)
+    {
+      if (sequence.Count <= maxTerms)
+      {
+        return string.Join(',', sequence);
+      }
+      return string.Concat(string.Join(',', sequence.Take(maxTerms)),",...");
+    }
+  }
+}
