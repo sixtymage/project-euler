2f8115850b96cca611051bfa517908c38195de32
Jon Hutchings <sixty.mage@outlook.com>
2021-12-28 11:35:32 +0200

    Basic addition support

---
+++ problem-13/BigDecimal.cs
@@ -0,0 +1,130 @@
+﻿namespace problem_13
+{
+  internal class BigDecimal
+  {
+    public int Count
+    {
+      get { return _digits.Count; }
+    }
+
+    public int this [int index]
+    {
+      get { return _digits[index]; }
+    }
+
+    private readonly List<int> _digits = new();
+
+    public BigDecimal()
+      : this("0")
+    {
+    }
+
+    public BigDecimal(string digits)
+    {
+      foreach (char c in digits)
+      {
+        _digits.Add(ToInt32(c));
+      }
+    }
+
+    private BigDecimal(List<int> digits)
+    {
+      _digits = digits;
+    }
+
+    public static BigDecimal operator +(BigDecimal a, BigDecimal b)
+    {
+      int i = a.Count - 1;
+      int j = b.Count - 1;
+      int carry = 0;
+
+      var digits = new List<int>();
+
+      while(i >= 0 || j >= 0)
+      {
+        var digitA = i >= 0 ? a[i] : 0;
+        var digitB = j >= 0 ? b[j] : 0;
+
+        var sum = digitA + digitB + carry;
+        carry = sum / 10;
+        var digit = sum % 10;
+
+        digits.Add(digit);
+
+        i--;
+        j--;
+      }
+
+      if (carry > 0)
+      {
+        digits.Add(carry);
+      }
+
+      digits = digits.Reverse<int>().ToList();
+      return new BigDecimal(digits);
+    }
+
+    public override string ToString()
+    {
+      return new string(_digits.Select(b => ToChar(b)).ToArray());
+    }
+
+    private static char ToChar(int digit)
+    {
+      switch (digit)
+      {
+        case 0:
+          return '0';
+        case 1:
+          return '1';
+        case 2:
+          return '2';
+        case 3:
+          return '3';
+        case 4:
+          return '4';
+        case 5:
+          return '5';
+        case 6:
+          return '6';
+        case 7:
+          return '7';
+        case 8:
+          return '8';
+        case 9:
+          return '9';
+      }
+
+      throw new OverflowException();
+    }
+
+    private static byte ToInt32(char digit)
+    {
+      switch (digit)
+      {
+        case '0':
+          return 0;
+        case '1':
+          return 1;
+        case '2':
+          return 2;
+        case '3':
+          return 3;
+        case '4':
+          return 4;
+        case '5':
+          return 5;
+        case '6':
+          return 6;
+        case '7':
+          return 7;
+        case '8':
+          return 8;
+        case '9':
+          return 9;
+      }
+
+      throw new OverflowException();
+    }
+  }
+}
--- problem-13/Program.cs
+++ problem-13/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace problem_13
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var a = new BigDecimal("1234");
+      var b = new BigDecimal("8962");
+
+      var c = a + b;
+      
+      Console.WriteLine($"{a} + {b} = {c}");
+    }
+  }
+}
