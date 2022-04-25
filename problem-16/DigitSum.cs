a9e4b10bdf3aef5bcc10ba4182abecc48eb95a5f
Jon Hutchings <sixty.mage@outlook.com>
2022-04-25 17:11:16 +0200

    Add solution for problem-16

---
+++ problem-16/DigitSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_16
+{
+  internal static class DigitSum
+  {
+    public static string FindSum(int number, uint exponent)
+    {
+      if (exponent == 0)
+      {
+        return $"{number}^{exponent} = 1 = 1";
+      }
+
+      var digits = new List<int> { number };
+
+      for (uint c = 1; c<exponent; c++)
+      {
+        var nextDigits = new List<int>();
+        var carry = 0;
+        foreach (var digit in digits)
+        {
+          var next = digit * number + carry;
+          nextDigits.Add(next % 10);
+          carry = next / 10;
+        }
+
+        if (carry != 0)
+        {
+          nextDigits.Add(carry);
+        }
+
+        digits = nextDigits;
+      }
+
+      digits.Reverse();
+      return $"{number}^{exponent} = {string.Join("", digits)} = {digits.Sum()}";
+    }
+  }
+}
--- problem-16/Program.cs
+++ problem-16/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using problem_16;
+
 Console.WriteLine("Hello, World!");
+
+for (int i = 0; i<=1000; i++)
+{
+  string sum = DigitSum.FindSum(2, (uint)i);
+  Console.WriteLine(sum);
+}
